49b20dc5c2c78df735817c435ae804b8765946f4
CTLIU <chiatungliu@gmail.com>
2017-10-14 12:02:15 +0800

    Fixed rubocop deflections for repo_info

--- lib/repo_info.rb
+++ lib/repo_info.rb
@@ -1,29 +1,26 @@
 require 'http'
 require 'yaml'
 
-#source = ['abc-news-au', 'the-economist', 'techradar']
+# source = ['abc-news-au', 'the-economist', 'techradar']
 config = YAML.safe_load(File.read('config/secrets.yml'))
 
 news_reponse = {}
 news_result = {}
 
-def news_api_path(source = 'techradar', sortBy = 'top') #create url where News you want
-	'https://newsapi.org/v1/articles?source=' + source + '&sortBy=' + sortBy
+def news_api_path(source = 'techradar', sort_by = 'top')
+  'https://newsapi.org/v1/articles?source=' + source + '&sort_by=' + sort_by
 end
 
-def call_news_url(config, url) #use key and dump raw data
-	HTTP.headers("x-api-key" => config['news_token']).get(url)
+def call_news_url(config, url) # use key and dump raw data
+  HTTP.headers('x-api-key' => config['news_token']).get(url)
 end
 
 repo_url = news_api_path('techradar', 'latest')
 news_reponse[repo_url] = call_news_url(config, repo_url)
-repo = news_reponse[repo_url].parse #turn to hash class
-news_result['status'] = repo['status'] 
+repo = news_reponse[repo_url].parse # turn to hash class
+news_result['status'] = repo['status']
 news_result['source'] = repo['source']
 news_result['sortBy'] = repo['sortBy']
 news_result['articles'] = repo['articles']
 
-File.write('spec/fixtures/result.yml', news_result.to_yaml) 
-
-
-
+File.write('spec/fixtures/result.yml', news_result.to_yaml)
